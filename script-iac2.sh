9393ba8c29b60ca58831e2d2ed9a0388aab17932
alanmaxs <alan.ciencias@gmail.com>
2022-08-19 12:01:29 +0000

    apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo " Instalando programas e atualizandfo o servidor"
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "copiando arquivos de uma aplicação"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
